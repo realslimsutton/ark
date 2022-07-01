3b6bf38bb4b36d5d78e25f4e8b49ff4b85ea18d0
Kieron Sutton <sutton.kieron99@gmail.com>
2022-07-01 14:30:38 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: [ 'Inter', ...defaultTheme.fontFamily.sans ]
+                sans: [ 'Barlow', ...defaultTheme.fontFamily.sans ]
             },
             colors: {
                 primary: {
