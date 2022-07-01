1fd6ea33fe4ca4c7e2e1741cdcaf138573e98530
Kieron Sutton <sutton.kieron99@gmail.com>
2022-07-01 23:02:33 +0100

    Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <x-layout.container>
-        <div class="h-96 w-full bg-primary-dark/90"></div>
+        <div class="h-96 w-full bg-primary-dark/90 rounded"></div>
 
         <div class="grid md:grid-cols-3 gap-6 mt-6">
-            <div class="h-24 w-full bg-primary-default shadow rounded-lg"></div>
-            <div class="h-24 w-full bg-primary-default shadow rounded-lg"></div>
-            <div class="h-24 w-full bg-primary-default shadow rounded-lg"></div>
+            <div class="h-24 w-full bg-primary-default shadow rounded"></div>
+            <div class="h-24 w-full bg-primary-default shadow rounded"></div>
+            <div class="h-24 w-full bg-primary-default shadow rounded"></div>
         </div>
     </x-layout.container>
 @endsection
