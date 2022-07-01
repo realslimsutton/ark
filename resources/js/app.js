4b78fcced01d41358fbdd94b918ef79913466387
Kieron Sutton <sutton.kieron99@gmail.com>
2022-07-02 00:38:01 +0100

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,14 @@
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
+import Glide, { Controls, Autoplay, Images } from '@glidejs/glide/dist/glide.modular.esm';
+import '@glidejs/glide/dist/css/glide.core.css';
+import '@glidejs/glide/dist/css/glide.theme.css';
+
+new Glide('.glide', {
+    type: 'carousel',
+    autoplay: 5000,
+    focusAt: 'center'
+}).mount({ Controls, Autoplay, Images });
 
 Alpine.plugin(collapse);
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,27 @@
 
 @section('content')
     <x-layout.container>
-        <div class="h-96 w-full bg-primary-dark/90 rounded"></div>
+        <div class="h-96 w-full bg-primary-dark/90 rounded glide">
+            <div class="glide__track" data-glide-el="track">
+                <ul class="glide__slides">
+                    <li class="glide__slide">
+                        <img src="{{asset('images/Slider3.jpg')}}" class="h-96 w-full object-cover rounded"/>
+                    </li>
+                    <li class="glide__slide">
+                        <img src="{{asset('images/Slider1.jpg')}}" class="h-96 w-full object-cover rounded"/>
+                    </li>
+                    <li class="glide__slide">
+                        <img src="{{asset('images/Slider2.png')}}" class="h-96 w-full object-cover rounded"/>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="glide__bullets" data-glide-el="controls[nav]">
+                <button class="glide__bullet" data-glide-dir="=0"></button>
+                <button class="glide__bullet" data-glide-dir="=1"></button>
+                <button class="glide__bullet" data-glide-dir="=2"></button>
+            </div>
+        </div>
 
         <div class="grid md:grid-cols-3 gap-6 mt-6">
             <div class="h-24 w-full bg-primary-default shadow rounded"></div>
