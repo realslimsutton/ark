6f1ddcc8330bc90d26f76f1eb471565783f49be4
Kieron Sutton <sutton.kieron99@gmail.com>
2022-07-02 00:47:43 +0100

    Update

--- resources/views/components/page/logo.blade.php
+++ resources/views/components/page/logo.blade.php
@@ -2,5 +2,5 @@
     href="#"
     class="text-white font-semibold text-4xl inline-flex items-center justify-center transition duration-150 hover:text-secondary-light focus:text-secondary-dark"
 >
-    Arkanda
+    SurviveOurArk
 </a>
