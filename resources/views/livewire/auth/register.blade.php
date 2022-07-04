8785003e84f2f9067ac836b1f8059a6caaaf2d09
Kieron Sutton <sutton.kieron99@gmail.com>
2022-07-04 16:33:15 +0100

    Updates

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,5 +1,5 @@
 <div class="min-h-screen w-full bg-primary-dark flex items-center justify-center">
-    <div class="max-w-md w-full px-4">
+    <div class="max-w-md w-full p-4">
         <form wire:submit.prevent="authenticate" class="space-y-24">
             <div class="flex items-center justify-center">
                 <a
