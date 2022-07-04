cb1c0258b28377640944586aab065935b9f91e1e
Kieron Sutton <sutton.kieron99@gmail.com>
2022-07-04 07:50:21 +0100

    Updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,11 +6,21 @@ use Filament\Facades\Filament;
 use Illuminate\Foundation\Vite;
 use Illuminate\Support\HtmlString;
 use Illuminate\Support\ServiceProvider;
+use Spatie\CpuLoadHealthCheck\CpuLoadCheck;
+use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+use Spatie\Health\Facades\Health;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->setupHealthChecks();
+
         Filament::serving(function () {
             Filament::registerNavigationGroups([
                 'News',
@@ -30,5 +40,25 @@ class AppServiceProvider extends ServiceProvider
                 new HtmlString('<link rel="icon" href="' . asset('images/favicon.ico') . '" type="image/x-icon">')
             ]);
         });
+    }
+
+    private function setupHealthChecks(): void
+    {
+        $checks = [
+            UsedDiskSpaceCheck::new(),
+            DatabaseCheck::new(),
+            CacheCheck::new(),
+            ScheduleCheck::new(),
+            DebugModeCheck::new(),
+            EnvironmentCheck::new()
+        ];
+
+        if (function_exists('sys_getloadavg')) {
+            $checks[] = CpuLoadCheck::new()
+                ->failWhenLoadIsHigherInTheLast5Minutes(2.0)
+                ->failWhenLoadIsHigherInTheLast15Minutes(1.5);
+        }
+
+        Health::checks($checks);
     }
 }
