90ac157e4412cb5bd43e8306c4b54d0394d4a5dc
Kieron Sutton <sutton.kieron99@gmail.com>
2022-07-09 10:45:55 +0100

    Updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,4 +17,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('01:30');
     }
+
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+    }
 }
